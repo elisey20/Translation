75542bebe89408c3b7d39d427376f97e64572dab
Елисей Ли <elisey.li.2002@mail.ru>
2020-11-26 01:56:46 +1000

    Начало

---
+++ main.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include "Structs.h"
+
+
+using namespace std;
+
+int main()
+{
+	char* str = new char[1000];
+
+	cin.getline(str, 1000);
+
+	bool flag = true;
+	unsigned short i = 0;
+	
+	//А - п .. р - ё
+
+	cout << 'А' << "\t" << (unsigned char)'А' << endl;
+
+	/*while (flag) {
+		if (str[i] > )
+	}*/
+
+	return 0;
+
+}
