f840d385b4507b98847f1fae2221fc099f41693c
Елисей Ли <elisey.li.2002@mail.ru>
2020-11-26 02:49:52 +1000

    Добавил файл структур (уже точно)

--- main.cpp
+++ main.cpp
@@ -6,30 +6,32 @@ using namespace std;
 
 int main()
 {
+	
+	setlocale(LC_ALL, "");
+	
 	char* str = new char[1000];
-
-	setlocale(LC_ALL, "Russian");
-
+	
 	cin.getline(str, 1000);
 
 	bool flag = true;
 	unsigned short i = 0;
-	
-	//А - п .. р - ё
-
-
 
 	while (flag) {
 		if ((str[i] >= -64 && str[i] <= -1) || str[i] == -88 || str[i] == -72) {
 			char j = 0;
 			while (j < 64) {
-				if (rus[j] == str[i])
+				if (rus[j] == str[i]) {
 					cout << eng[j];
+					j = 64;
+				}
+				j++;
 			}
 		}
 		else
 			cout << str[i];
 		i++;
+		if (str[i] == '\0')
+			flag = false;
 	} 
 
 	return 0;
